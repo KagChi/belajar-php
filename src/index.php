45c5c0b7071ec8fa238af18a82aea3a601b98ba7
kagchi <yonuxbot@gmail.com>
2023-09-21 18:30:43 +0700

    feat: initial design

--- src/index.php
+++ src/index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
 <body>
-
-<?php
-    echo "My first PHP script!";
-?>
-
+    <div class="bg-[#27005D] w-full h-16">
+    </div>
+    <div class="pt-2 pb-2">
+        <div class="mr-6 bg-[#071952] w-16 h-screen rounded-r-md" />
+    </div>
 </body>
 </html>
